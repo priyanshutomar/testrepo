387c93c754c7347d047298c54aea4ef6e24f6ca1
Priyanshu Tomar <priyanshu.tomar@appdirect.com>
2018-09-05 14:53:17 +0530

    Update util.groovy

--- util.groovy
+++ util.groovy
@@ -1,5 +1,20 @@
-def call(String whoAreYou) {
-    echo "Now we're being called more magically, ${whoAreYou}, thanks to the call(...) method."
+def call(String filePath) {
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cd286c7e-b875-4a15-b0e5-e54f0ec6469a', url: 'git@github.com:priyanshutomar/testrepo2.git']]])
+    def content = new File(pwd() + "/csvFile.csv").readLines()
+    content.eachWithIndex { String line, int i ->
+        def vals = line.split(',', -1)
+        def params = []
+        vals.eachWithIndex { String val, int j ->
+            if (!(val == null || val == "")) {
+                params.add(string(name: "${content.get(0).split(',', -1)[j]}", value: "${vals[j]}"))
+            }
+        }
+        if (i != 0) { 
+            stage(vals[0]) {
+                build job: 'slaveJob', parameters: params
+            }
+        }
+    }
 }
 
 return this;
