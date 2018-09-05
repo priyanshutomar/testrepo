bb1df3b5616e4553c3ddcd626a7f9be6a0fb01bd
Priyanshu Tomar <priyanshu.tomar@appdirect.com>
2018-09-05 14:53:58 +0530

    Update util.groovy

--- util.groovy
+++ util.groovy
@@ -1,6 +1,6 @@
 def call(String filePath) {
     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cd286c7e-b875-4a15-b0e5-e54f0ec6469a', url: 'git@github.com:priyanshutomar/testrepo2.git']]])
-    def content = new File(pwd() + "/csvFile.csv").readLines()
+    def content = new File(filePath).readLines()
     content.eachWithIndex { String line, int i ->
         def vals = line.split(',', -1)
         def params = []
